33f0d46d961ee53caad7e678b793f275877344da
Seoyoung Kim <mintholic1@gmail.com>
2020-04-20 20:31:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 def mario
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a me, Mario!"
+  $phrase = "It's-a me, Mario!"
   puts phrase
 end
 
@@ -10,6 +10,13 @@ end
 
 
 def link
-  link_phrase = "It's Dangerous To Go Alone! Take This."
+  $link_phrase = "It's Dangerous To Go Alone! Take This."
   puts link_phrase
+end
+
+
+def all_phrases
+  puts $status
+  puts $phrase
+  puts $link_phrase
 end
